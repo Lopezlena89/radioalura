88d3079bfe4de863a08b78cd94921fa2bf0c7d09
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2024-04-15 22:52:16 -0600

    polimorfismo

--- src/com/aluracursos/radioalura/principal/Principal.java
+++ src/com/aluracursos/radioalura/principal/Principal.java
@@ -1,6 +1,7 @@
 package com.aluracursos.radioalura.principal;
 
 import com.aluracursos.radioalura.modelos.Cancion;
+import com.aluracursos.radioalura.modelos.MisFavoritos;
 import com.aluracursos.radioalura.modelos.Podcast;
 
 public class Principal {
@@ -14,16 +15,27 @@ public class Principal {
         miPodcast.setPresentador("Gabriela Agiar");
         miPodcast.setTitulo("Cafe.Tech");
 
-
+        //Cancion
         for (int i = 0; i < 100; i++) {
             miCancion.meGusta();
         }
-        for (int i = 0; i < 5000; i++) {
+        for (int i = 0; i < 200; i++) {
             miCancion.reproduce();
+        }
+        //Podcast
+        for (int i = 0; i < 100; i++) {
+            miPodcast.meGusta();
+        }
+        for (int i = 0; i < 8000; i++) {
+            miPodcast.reproduce();
         }
 
         System.out.println("Total reproducciones " + miCancion.getTotalDeReproducciones());
         System.out.println("Total de me gusta " + miCancion.getTotalDeMeGusta());
+
+        MisFavoritos favoritos = new MisFavoritos();
+        favoritos.adicione(miPodcast);
+        favoritos.adicione(miCancion);
 
     }
 }
